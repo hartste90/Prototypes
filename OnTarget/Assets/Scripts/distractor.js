36585a79fea52c3e3fa4615ce2186a0efebf636e
hartste90 <steveh@lumoslabs.com>
2015-09-01 10:52:31 -0700

    Added HUD

--- OnTarget/Assets/Scripts/distractor.js
+++ OnTarget/Assets/Scripts/distractor.js
@@ -33,7 +33,7 @@ function ApplyDamage ( damage: float )
 		var canvas = recap.GetComponent(Canvas);
 		canvas.enabled = true;
 		//disable the Next button since they failed the level
-		GameObject.Find("Next").GetComponent(UI.Button).interactable = false;
+		GameObject.Find("Next Level Button").GetComponent(UI.Button).interactable = false;
 	}
 	gameObject.GetComponent(SpriteRenderer).color.b -= .1;
 	gameObject.GetComponent(SpriteRenderer).color.g -= .1;

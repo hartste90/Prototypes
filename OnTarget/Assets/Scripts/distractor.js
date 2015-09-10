a39d201d55c7df2a4c7ec37302cf051720c97f14
Steve Hart <hartste90@gmail.com>
2015-09-10 00:37:12 -0700

    Revamping distractor as inherited class

--- OnTarget/Assets/Scripts/distractor.js
+++ OnTarget/Assets/Scripts/distractor.js
@@ -1,32 +1,73 @@
-﻿
+﻿#pragma strict
 
-var health : float;
-var tint_time : float;
-
-
-
-function Start () {
-}
-
-function Update () {
-
-
-}
-
-function OnCollisionEnter2D( coll: Collision2D ) 
+public class Distractor extends MonoBehaviour
 {
-	if (coll.gameObject.tag == "Bullet")
+
+    public var health : float;
+	public var tint_timer : float;
+	public var lerp_speed : float = 2;
+	public var sound_hit: AudioClip;
+	public var sound_explode: AudioClip; 
+	public var original_position: Vector3;
+    
+    public function Distractor()
+    {
+        health = 100;
+    }
+    public function Distractor(hp : int)
+    {
+        health = hp;
+    }
+   
+	public function Start () 
 	{
-		ApplyDamage(10.0f);
-		Destroy(coll.gameObject); 
+		tint_timer = Time.time;
+		original_position = transform.position;
 	}
-		
-}
+	public function Update () 
+	{
+		if (Time.time > tint_timer)
+		{
+			gameObject.GetComponent(SpriteRenderer).color = new Color(1,1,1,1); 
+		}
+		if (transform.position != original_position)
+		{
+			transform.position = Vector2.Lerp(transform.position, original_position, Time.deltaTime * lerp_speed);
+		}
+	}
 
-function ApplyDamage ( damage: float )
-{
-	health -= damage;
-	if (health <= 0)
+	public function OnCollisionEnter2D( coll: Collision2D ) 
+	{
+		if (coll.gameObject.tag == "Bullet")
+		{
+			Destroy(coll.gameObject);
+			//visual target feedback
+			transform.position.y = transform.position.y + Random.Range(.05, .15);
+			//audio feedback
+			transform.GetComponent(AudioSource).clip = sound_hit;
+			transform.GetComponent(AudioSource).Play();
+			//apply damage in model
+			ApplyDamage(10.0f);
+			
+			 
+		}
+			
+	}
+
+	public function ApplyDamage ( damage: float )
+	{
+		health -= damage;
+		if (health <= 0)
+		{
+			Explode();
+		}
+		gameObject.GetComponent(SpriteRenderer).color.b -= .1;
+		gameObject.GetComponent(SpriteRenderer).color.g -= .1;
+
+		
+	}
+	
+	public function Explode ()
 	{
 		Destroy(gameObject);
 		var recap = GameObject.Find("Recap Menu");
@@ -34,9 +75,8 @@ function ApplyDamage ( damage: float )
 		canvas.enabled = true;
 		//disable the Next button since they failed the level
 		GameObject.Find("Next Level Button").GetComponent(UI.Button).interactable = false;
-	}
-	gameObject.GetComponent(SpriteRenderer).color.b -= .1;
-	gameObject.GetComponent(SpriteRenderer).color.g -= .1;
 
-	
+	}
+    
 }
+

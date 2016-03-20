1551a483ca283def228db25b74b1fb9ae9077fba
hartste90 <hartste90@gmail.com>
2016-03-19 22:59:06 -0700

    added cursor stuff

--- Bubbles/Assets/Code/Bubble.cs
+++ Bubbles/Assets/Code/Bubble.cs
@@ -20,46 +20,47 @@ public class Bubble : MonoBehaviour
 	
 	}
 
-	public void OnMouseUp()
-	{
-		Debug.Log ("Clicked bubble");
-	}
-
-	public void OnMouseOver()
-	{
-
-		GameObject effect = Instantiate (UIManager.instance.bubblePopEffectPrefab, transform.position, Quaternion.identity) as GameObject;
-		effect.GetComponent<ParticleSystem>().startColor = bubble.color;
-		if (UIManager.instance.GetCurrentColor() == bubble.color)
-		{
-			UIManager.instance.IncreaseScore (100);
-			UIManager.instance.AddTime();
-		}
-		else
-		{
-			UIManager.instance.DecreaseScore(200);
-			UIManager.instance.SetCurrentColor(bubble.color);
-			UIManager.instance.ResetCount();
-			UIManager.instance.SubtractTime();
-		}
-
-		UIManager.instance.IncreaseCount (1);
-		if ( UIManager.instance.GetCount() >= 5 )
-		{
-			UIManager.instance.ResetCount();
-			UIManager.instance.IncreaseScore (500);
-		}
-
-		Destroy (gameObject);
-	}
-
 	void OnCollisionEnter2D(Collision2D coll)
 	{
 		if (coll.gameObject.tag == "Boundary")
 		{
 			Destroy(gameObject);
 		}
+		else if (coll.gameObject.tag == "Cursor")
+		{
+			OnCursorTouch();
+		}
 	}
+
+	protected void OnCursorTouch ()
+	{
+		GameObject effect = Instantiate (UIManager.instance.bubblePopEffectPrefab, transform.position, Quaternion.identity) as GameObject;
+		effect.GetComponent<ParticleSystem>().startColor = bubble.color;
+		if (UIManager.instance.GetCurrentColor() == bubble.color)
+		{
+			UIManager.instance.IncreaseScore (100);
+			UIManager.instance.AddTime(1.0f);
+		}
+		else
+		{
+			UIManager.instance.DecreaseScore(200);
+			UIManager.instance.SetCurrentColor(bubble.color);
+			UIManager.instance.ResetCount();
+			UIManager.instance.SubtractTime(.5f);
+		}
+		
+		UIManager.instance.IncreaseCount (1);
+		if ( UIManager.instance.GetCount() >= 5 )
+		{
+			UIManager.instance.ResetCount();
+			UIManager.instance.IncreaseScore (500);
+		}
+		
+		Destroy (gameObject);
+	}
+
+
+
 
 
 
---
+++ Bubbles/Assets/Code/Cursor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cursor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButton(0))
+		{
+			MoveToTouch();
+		}
+	}
+
+	protected void MoveToTouch()
+	{
+		Vector3 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+//		pos.y = pos.y + 20f;
+		pos.z = 0;
+		transform.localPosition = pos;
+
+
+	}
+}

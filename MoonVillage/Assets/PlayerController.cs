62050e6ce6242679701a66891a51ab4ab44f5e0c
hartste90 <steveh@lumoslabs.com>
2016-05-04 11:33:29 -0700

    update

---
+++ MoonVillage/Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	protected Rigidbody2D rigidbody;
+
+	public float JUMP_FORCE = 100f;
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody2D>();
+		rigidbody.AddForce(new Vector2(400f,0f));
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			Debug.Log("Pressed left click.");
+			rigidbody.AddForce(new Vector2 (0f, JUMP_FORCE));
+		}
+	}
+
+	void OnMouseDown()
+	{
+		Debug.Log("JUPMIN");
+	}
+}

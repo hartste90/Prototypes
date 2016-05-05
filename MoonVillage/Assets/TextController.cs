8c49c846166d24fcd82c05d634ee6efe1f8ffef1
hartste90 <steveh@lumoslabs.com>
2016-05-04 19:20:55 -0700

    adding assets

---
+++ MoonVillage/Assets/TextController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class TextController : MonoBehaviour {
+
+	public List<string> dialogue = new List<string>();
+
+	// Use this for initialization
+	void Start () {
+	
+		dialogue.Add("Being a mom is one tough job...");
+		dialogue.Add("Being a mom is one tough job...");
+		dialogue.Add("Being a mom is one tough job...");
+		dialogue.Add("Being a mom is one tough job...");
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

3965dc2d7986cf88d65b5da875778090067ed098
hartste90 <hartste90@gmail.com>
2016-07-24 17:25:48 -0700

    working pie chart

---
+++ CircleMaze/Assets/PieGraph.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PieGraph : MonoBehaviour {
+
+	[SerializeField]
+	protected float[] amounts;
+	[SerializeField]
+	protected Color[] colors;
+	[SerializeField]
+	protected Image wedgePrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+		MakeGraph ();
+	}
+
+	protected void MakeGraph()
+	{
+		float total = 0f;
+		float zRotation = 0f;
+		foreach (float value in amounts) 
+		{
+			total += value;
+		}
+		for (int i = 0; i < amounts.Length; i++)
+		{
+			Image newWedge = Instantiate(wedgePrefab) as Image;
+			newWedge.transform.SetParent (transform, false);
+			newWedge.color = colors[i];
+			newWedge.fillAmount = amounts[i] / total;
+			newWedge.transform.rotation = Quaternion.Euler( new Vector3 (0f, 0f, zRotation));
+			zRotation -= newWedge.fillAmount * 360f;
+		}
+
+	}
+
+
+}

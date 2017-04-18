f1f8896064179a401c73a910686a7ee1cc5a0b4b
hartste90 <hartste90@gmail.com>
2017-04-18 09:31:10 -0700

    new Bub4

---
+++ Unity Personal/Bub4/Assets/Scripts/Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	public GameObject borderItemPrefab;
+	// Use this for initialization
+	void Start () {
+		//create grid of spaces on play area
+		Debug.Log(Screen.width + ", " + Screen.height);
+
+		Vector3 p = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width/2, Screen.height/2, Camera.main.nearClipPlane));
+
+		Instantiate(borderItemPrefab, p, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

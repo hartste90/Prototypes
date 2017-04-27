addc89d1e38a673f1f487292b6d815e17d707073
Steve Hart <steveh@01421-mbp.local>
2017-04-27 13:42:18 -0700

    BUM: added screenshot script

---
+++ Bumpers/Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public Vector3 direction = Vector3.forward;
+	public CharacterController characterController;
+
+	// Use this for initialization
+	void Start () {
+	        characterController = GetComponent <CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveInCurrentDirection();
+	}
+
+	public void MoveInCurrentDirection()
+	{
+//		characterController.Move((transform.rotation * motion) * Time.deltaTime);
+
+	}
+}

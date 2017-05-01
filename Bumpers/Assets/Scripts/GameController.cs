6c747cb178f6d3782aaa2ba2670fd6237af45e9f
hartste90 <hartste90@gmail.com>
2017-04-30 20:50:38 -0700

    BUM: spawning by code

--- Bumpers/Assets/Scripts/GameController.cs
+++ Bumpers/Assets/Scripts/GameController.cs
@@ -4,5 +4,42 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-	public float gameSpeed = 1;
+	public int numStartingMines;
+	public int numStartingBumpers; 
+	public int numStartingCoins; 
+
+	public float gameSpeed;
+	public GameObject coinPrefab;
+	public GameObject minePrefab;
+
+	public void Awake()
+	{
+		Tools.screenWidth = Screen.width;
+		Tools.screenHeight = Screen.height;
+	}
+
+
+	void Start()
+	{
+		//spawn starting coin
+		SpawnMultiple(1, coinPrefab);
+		//spawn starting mines
+		SpawnMultiple(5, minePrefab);
+	}
+
+	public void SpawnMultiple (int numToSpawn, GameObject gameObject)
+	{
+		for (int i = 0; i < numToSpawn; i++) 
+		{
+			Debug.Log (i);
+			SpawnGameObject (gameObject);
+		}
+	}
+
+	public void SpawnGameObject (GameObject gameObject)
+	{
+		Vector3 screenPosition = (new Vector3(Random.Range(-15,15), 0, Random.Range(-15,15)));
+		Instantiate(gameObject,screenPosition,Quaternion.identity);
+	}
+
 }
---
+++ Bumpers/Assets/Scripts/Tools.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Tools
+{
+
+	public static int screenWidth;
+	public static int screenHeight;
+
+}

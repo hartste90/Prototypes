00f2183103852acad6360b2c80e5b3c209af52cc
Steve Hart <steveh@01421-mbp-18.local>
2017-10-11 17:38:35 -0700

    BUM: added coin spritesheet

--- Bumpers/Assets/Scripts/Rotate.cs
+++ Bumpers/Assets/Scripts/Rotate.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotate : MonoBehaviour {
-	
+
+	public Vector3 rotateBy;
+	public float speed;
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (Vector3.up * 3);
+		transform.Rotate (rotateBy * speed);
 		
 	}
 }
